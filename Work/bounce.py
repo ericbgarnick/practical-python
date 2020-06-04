0ec99670b8b45cf913ae6656c4740fff5f241169
Eric Garnick <eric@energysage.com>
2020-06-04 05:57:39 -0400

    Exercise 1.5 bounce

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+REBOUND = 0.6
+
+
+if __name__ == "__main__":
+    height = 100.0
+    num_bounces = 10
+    for i in range(num_bounces):
+        height *= REBOUND
+        print(f"{i + 1} {round(height, 4)}")
